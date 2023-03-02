45eb0fb5d83ba4144a402f535aa01d79a39af576
ndt <ndneiht1200.relax@gmail.com>
2023-03-02 15:04:59 +0300

    project upload

--- src/App.js
+++ src/App.js
@@ -1,25 +1,55 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
 
-function App() {
+const App = () => {
+  const [turn, setTurn] = useState(false);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>Turn: {turn ? "O" : "X"}</h1>
+      <div className="board-row">
+        <Square turn={turn} setTurn={setTurn} />
+        <Square turn={turn} setTurn={setTurn} />
+        <Square turn={turn} setTurn={setTurn} />
+      </div>
+      <div className="board-row">
+        <Square turn={turn} setTurn={setTurn} />
+        <Square turn={turn} setTurn={setTurn} />
+        <Square turn={turn} setTurn={setTurn} />
+      </div>
+      <div className="board-row">
+        <Square turn={turn} setTurn={setTurn} />
+        <Square turn={turn} setTurn={setTurn} />
+        <Square turn={turn} setTurn={setTurn} />
+      </div>
     </div>
   );
-}
+};
 
 export default App;
+
+const Square = ({turn, setTurn}) => {
+  const [value, setValue] = useState({
+    value: null,
+    isClick: false
+  });
+
+  const clickHandle = () => {
+    if (!value.isClick) {
+        if (turn) {
+            setValue({value: "O", isClick: true})
+        }
+        else {
+            setValue({value: "X", isClick: true})
+        }
+        setTurn(!turn)
+    }
+  };
+
+  return (
+    <div>
+      <button className="square" onClick={clickHandle}>
+        {value.value}
+      </button>
+    </div>
+  );
+};
