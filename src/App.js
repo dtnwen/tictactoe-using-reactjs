64583225bd447f7e133cc5fa8b7d62443bb49f63
ndt <ndneiht1200.relax@gmail.com>
2023-03-02 18:00:17 +0300

    fixing error

--- src/App.js
+++ src/App.js
@@ -2,25 +2,26 @@ import React, { useState } from "react";
 
 const App = () => {
   const [turn, setTurn] = useState(false);
-    const [value, setValue] = useState()
+    const [square, setSquare] = useState(Array(9).fill({value: null, isClick: false}))
+    console.log(square)
 
   return (
     <div>
       <h1>Turn: {turn ? "O" : "X"}</h1>
       <div className="board-row">
-        <Square turn={turn} setTurn={setTurn} />
-        <Square turn={turn} setTurn={setTurn} />
-        <Square turn={turn} setTurn={setTurn} />
+        <Square turn={turn} setTurn={setTurn} square={square[0]} setSquare={setSquare} />
+        <Square turn={turn} setTurn={setTurn} square={square[1]} setSquare={setSquare} />
+        <Square turn={turn} setTurn={setTurn} square={square[2]} setSquare={setSquare} />
       </div>
       <div className="board-row">
-        <Square turn={turn} setTurn={setTurn} />
-        <Square turn={turn} setTurn={setTurn} />
-        <Square turn={turn} setTurn={setTurn} />
+        <Square turn={turn} setTurn={setTurn} square={square[3]} setSquare={setSquare} />
+        <Square turn={turn} setTurn={setTurn} square={square[4]} setSquare={setSquare} />
+        <Square turn={turn} setTurn={setTurn} square={square[5]} setSquare={setSquare} />
       </div>
       <div className="board-row">
-        <Square turn={turn} setTurn={setTurn} />
-        <Square turn={turn} setTurn={setTurn} />
-        <Square turn={turn} setTurn={setTurn} />
+        <Square turn={turn} setTurn={setTurn} square={square[6]} setSquare={setSquare} />
+        <Square turn={turn} setTurn={setTurn} square={square[7]} setSquare={setSquare} />
+        <Square turn={turn} setTurn={setTurn} square={square[8]} setSquare={setSquare} />
       </div>
     </div>
   );
@@ -28,27 +29,24 @@ const App = () => {
 
 export default App;
 
-const Square = ({ turn, setTurn }) => {
-  const [value, setValue] = useState({
-    value: null,
-    isClick: false,
-  });
-
+const Square = ({ turn, setTurn, square, setSquare }) => {
+    console.log(square.isClick)
+    
   const clickHandle = () => {
-    if (!value.isClick) {
+    if (!square.isClick) {
       if (turn) {
-        setValue({ value: "O", isClick: true });
+        setSquare({ value: "O", isClick: true });
       } else {
-        setValue({ value: "X", isClick: true });
+        setSquare({ value: "X", isClick: true });
       }
       setTurn(!turn);
     }
-  };
+};
 
   return (
     <div>
       <button className="square" onClick={clickHandle}>
-        {value.value}
+        {}
       </button>
     </div>
   );
